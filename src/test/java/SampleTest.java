4ebab7952cf1a9283cb7d4c093b0c944657317a2
poojitha <poojitha@poojithas-MacBook-Pro.local>
2023-12-07 16:16:58 +0530

    Pooja

---
+++ src/test/java/SampleTest.java
@@ -0,0 +1,12 @@
+import org.testng.annotations.Test;
+
+public class SampleTest {
+    @Test
+    public void tese1(){
+        System.out.println("Hello");
+    }
+    @Test
+    public void tese2(){
+        System.out.println("Hello world");
+    }
+}
